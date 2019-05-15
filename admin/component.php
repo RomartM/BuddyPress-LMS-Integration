594ed419df3d662c7afa08aafad2c2e699130bf6
romart <mediantekid@gmail.com>
2019-05-15 13:11:03 +0800

    Backend component

---
+++ admin/component.php
@@ -0,0 +1,28 @@
+<?php
+// Admin Dashboard Assets
+function bp_lms_load_admin_stylesheet()
+{
+    wp_enqueue_style('jquery-ui-css', plugins_url('css/lib/jquery-ui.css', __FILE__));
+    wp_enqueue_style('snackbar-css', plugins_url('css/lib/snackbar.css', __FILE__));
+    wp_enqueue_style('material-icon-css', plugins_url('css/lib/icon.css', __FILE__));
+    wp_enqueue_style('bpe-admin-dashboard-styles', plugins_url('css/bp-lms-admin-dashboard.css', __FILE__));
+}
+
+function bp_lms_load_admin_javascript()
+{
+    wp_enqueue_script('jquery-ui-js', plugins_url('js/lib/jquery-ui.js', __FILE__), array('jquery'));
+    // Load SnackBar Plugin lib
+    wp_enqueue_script('bpe-admin-dashboard-snackbar', plugins_url('js/lib/snackbar.js', __FILE__), array('jquery'));
+    // Load BMI Dashboard script
+    wp_enqueue_script('bpe-admin-dashboard-script', plugins_url('js/bp-lms-admin-dashboard.js', __FILE__), array('jquery'));
+}
+
+
+function bp_lms_admin_dashboard(){
+    add_menu_page('BuddyPress LMS Integration Dashboard', 'BuddyPress LMS', 'manage_options', 'buddypress-lms-integration', 'bp_lms_admin_dashboard_init', '', 55);
+}
+
+// Plugin Admin Dashboard Initiator
+function bp_lms_admin_dashboard_init(){
+    require_once('dashboard.php');
+}
