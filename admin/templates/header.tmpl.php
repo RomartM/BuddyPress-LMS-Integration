469cf3ae04edc44b92b0ba3163218ecef45e1841
romart <mediantekid@gmail.com>
2019-05-28 19:22:32 +0800

    Plugin header template

---
+++ admin/templates/header.tmpl.php
@@ -0,0 +1,8 @@
+<header class="bp-lms-header" role="banner">
+    <div class="bp-lms-title">
+        <h1><?php echo $bp_lms_plugin_info['name'];?></h1>
+    </div>
+    <div class="bp-lms-description">
+        <p><?php echo $bp_lms_plugin_info['description']; ?></p>
+    </div>
+</header>
