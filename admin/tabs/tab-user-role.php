61b0ba75a57d02e534d1db5af3e7ce3d73c3bb95
romart <mediantekid@gmail.com>
2019-05-28 19:21:23 +0800

    Deprecated modules kay pede ra ma buhat sa WP Post Type

--- admin/tabs/tab-user-role.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Generate School Form
-$fields = new BP_LMS_XProfile_Field(xprofile_get_field_id_from_name('User Role'));
-$fields->tab = 'user-role';
-$fields->identifier = 'user-role';
-$fields->render_tab_form();
