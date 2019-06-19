4eab3452daae851794f297da084ca05dd024b7ca
romart <mediantekid@gmail.com>
2019-06-19 09:52:00 +0800

    Test Code Development Done and Fixed

--- admin/overrides/lp-quiz-start.php
+++ admin/overrides/lp-quiz-start.php
@@ -21,21 +21,22 @@ $quiz   = LP_Global::course_item_quiz();
 <?php do_action( 'learn-press/before-quiz-start-button' ); ?>
 
     <form name="start-quiz" class="start-quiz" method="post" enctype="multipart/form-data">
-
-        <span style="font-size:16px;font-weight:600;color:#E91E63;">
-            To start the exam, please provide the access code given by your instructor
-        </span>
-        <hr/>
-
         <?php do_action( 'learn-press/begin-quiz-start-button' ); ?>
 
-        <input type="text" name="access_code" placeholder="Access Code">
-
+        <?php
+        $access_code_data = get_post_meta($quiz->get_id(), '_bp_lms_test_code_meta_key');
+        if(!empty($access_code_data)):
+        ?>
+        <div style="font-size:16px;font-weight:600;color:#E91E63;margin-bottom: 15px;">
+            To start the exam, please provide the access code given by your instructor
+        </div>
+        <input type="text" name="access-code" placeholder="Access Code" required>
+        <?php endif;?>
         <button type="submit" class="button"><?php _e( 'Start', 'learnpress' ); ?></button>
 
         <?php do_action( 'learn-press/end-quiz-start-button' ); ?>
 
-        <?php LP_Nonce_Helper::quiz_action( 'start', $quiz->get_id(), $course->get_id(), true ); ?>
+        <?php LP_Nonce_Helper::quiz_action( 'start-custom', $quiz->get_id(), $course->get_id(), true ); ?>
         <input type="hidden" name="noajax" value="yes">
 
     </form>
--- includes/core.php
+++ includes/core.php
@@ -58,13 +58,12 @@ add_action( 'restrict_manage_posts', 'filter_by_schools_taxonomies');
 // Add User Dashboard ShortCode
 add_shortcode( 'user_dashboard', 'user_dashboard_shortcode' );
 
-function r(){
+function bp_lms_remove_start_button(){
     remove_action( 'learn-press/quiz-buttons', 'learn_press_quiz_start_button', 10 );
     function learn_press_quiz_start_button_custom() {
         $course = LP_Global::course();
         $user   = LP_Global::user();
         $quiz   = LP_Global::course_item_quiz();
-
         if ( $user->has_course_status( $course->get_id(), array( 'finished' ) ) || $user->has_quiz_status( array(
                 'started',
                 'completed'
@@ -79,7 +78,91 @@ function r(){
         include BP_LMS_BASE_PATH.'/admin/overrides/lp-quiz-start.php';
     }
     add_action( 'learn-press/quiz-buttons', 'learn_press_quiz_start_button_custom', 10 );
-
 }
 
-add_action('init', 'r');
+add_action('init', 'bp_lms_remove_start_button');
+
+
+function bp_lms_remove_start_quiz_hook(){
+
+    $start_quiz_arr = array(LP_Quiz_Factory::class, 'start_quiz');
+
+    remove_action( 'learn-press/ajax/start-quiz:nopriv', $start_quiz_arr, 5 );
+    remove_action( 'learn-press/ajax/no-priv/start-quiz:nopriv', $start_quiz_arr, 5 );
+    remove_action( 'learn_press_ajax_handler_start-quiz:nopriv', $start_quiz_arr, 5 );
+
+    remove_action( 'learn_press_request_handler_lp-start-quiz:nopriv', $start_quiz_arr, 5);
+
+    function start_quiz_custom() {
+        $access_code = LP_Request::get_int( 'access-code' );
+        $course_id = LP_Request::get_int( 'course-id' );
+        $quiz_id   = LP_Request::get_int( 'quiz-id' );
+
+        $access_code_data = get_post_meta($quiz_id, '_bp_lms_test_code_meta_key');
+
+        $user      = learn_press_get_current_user();
+        if(empty($access_code_data[0]) && empty($access_code)){
+            start_quiz_custom_core($quiz_id, $course_id, $user);
+        }else{
+            if($access_code===$access_code_data[0]){
+                start_quiz_custom_core($quiz_id, $course_id, $user);
+            }else{
+                $result['message']  = "Your access code is invalid";
+                $result['result']   = 'error';
+                $result['redirect'] = apply_filters( 'learn-press/quiz/start-quiz-failure-redirect', learn_press_get_current_url(), $quiz_id, $course_id, $user->get_id() );
+                start_quiz_custom_alert($result);
+            }
+        }
+    }
+
+    function start_quiz_custom_core($quiz_id, $course_id, $user){
+        $quiz      = learn_press_get_quiz( $quiz_id );
+        $result    = array( 'result' => 'success' );
+
+        try {
+            // Actually, no save question here. Just check nonce here.
+            $check = LP_Quiz_Factory::maybe_save_questions( 'start-custom' );
+
+            // PHP Exception
+            if ( learn_press_is_exception( $check ) ) {
+                throw $check;
+            }
+
+            $data = $user->start_quiz( $quiz_id, $course_id, true );
+            if ( is_wp_error( $data ) ) {
+                throw new Exception( $data->get_error_message() );
+            } else {
+
+                $redirect           = $quiz->get_question_link( learn_press_get_user_item_meta( $data['user_item_id'], '_current_question', true ) );
+                $result['result']   = 'success';
+                $result['redirect'] = apply_filters( 'learn-press/quiz/started-redirect', $redirect, $quiz_id, $course_id, $user->get_id() );
+            }
+        }
+        catch ( Exception $ex ) {
+            $result['message']  = $ex->getMessage();
+            $result['result']   = 'error';
+            $result['redirect'] = apply_filters( 'learn-press/quiz/start-quiz-failure-redirect', learn_press_get_current_url(), $quiz_id, $course_id, $user->get_id() );
+        }
+        start_quiz_custom_alert($result);
+    }
+
+    function start_quiz_custom_alert($result){
+        learn_press_maybe_send_json( $result );
+
+        if ( ! empty( $result['message'] ) ) {
+            learn_press_add_message($result['message'], $result['result']);
+        }
+
+        if ( ! empty( $result['redirect'] ) ) {
+            wp_redirect( $result['redirect'] );
+            exit();
+        }
+    }
+
+    $action = 'start-custom-quiz';
+    $custom_action  = 'start_quiz_custom';
+    LP_Request_Handler::register_ajax( $action, $custom_action );
+    LP_Request_Handler::register( "lp-{$action}", $custom_action );
+}
+
+add_action('init', 'bp_lms_remove_start_quiz_hook');
